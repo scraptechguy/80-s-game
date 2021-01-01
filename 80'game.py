035fc0944e39d3ff45e3bdbf0b8583268980da47
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-01-01 21:44:34 +0100

    Update 80'game.py

--- 80'game.py
+++ 80'game.py
@@ -21,27 +21,49 @@ def go_west():
     x_axis -= 1
     return x_axis
 
-print("You've just woken up. Confused lying the middle of a huge forest. You get up, but setting around you is not familiar"
-" at all. What other option that heading somewhere do you have? Let's hit the roud!")
 
-for i in range(200):
-    nsew = int(input("Where do you want to go? (N - 1/S - 2/E - 3/W - 4): "))
+def game():
+    print("You've just woken up. Confused lying the middle of a huge forest. You get up, but setting around you is not familiar"
+    " at all.\nWhat other option that heading somewhere do you have? Let's hit the roud!")
+    for i in range(200):
+        nsew = int(input("Where do you want to go? (N - 1/S - 2/E - 3/W - 4): "))
 
-    if nsew == 1:
-        go_north()
-    elif nsew == 2:
-        go_south()
-    elif nsew == 3:
-        go_east()
-    elif nsew == 4:
-        go_west
-    else:
-        print("Let's take that more seriously. Come on, input a direction.")
+        if nsew == 1:
+            go_north()
+            print("Y axis: " + str(y_axis))
+            print("X axis: " + str(x_axis))
+            
+        elif nsew == 2:
+            go_south()
+            print("Y axis: " + str(y_axis))
+            print("X axis: " + str(x_axis))
+
+        elif nsew == 3:
+            go_east()
+            print("Y axis: " + str(y_axis))
+            print("X axis: " + str(x_axis))
+
+        elif nsew == 4:
+            go_west()
+            print("Y axis: " + str(y_axis))
+            print("X axis: " + str(x_axis))
+
+        else:
+            print("Let's take that more seriously. Come on, input a direction.")
 
 
-    if y_axis == 1 and x_axis == 1:
-        print("Ou, your things. That's handy. You've got a lot of food, now you don't need to worry about it!")
-    elif y_axis == 12 and x_axis == 12:
-        print("Uuu, a bone fire. ")
-    else:
-        print("There's nothing...")
+        if y_axis == 1 and x_axis == 1:
+            print("Ou, your things. That's handy. You've got a lot of food, now you don't need to worry about it!")
+        elif y_axis == -1 and x_axis == -1:
+            print("Well, that was fast. You found a piece of paper with some numbers")
+        elif y_axis == 12 and x_axis == 12:
+            print("Uuu, a bone fire. ")
+        else:
+            print("There's nothing...")
+
+    restart = input("Oh, it seems you ran out of time. Never mind, do you want to play again? (y/n): ")
+    if restart == "y" or restart == "Y":
+        game()
+
+
+game()
