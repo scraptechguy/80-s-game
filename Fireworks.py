505b372e7a0e92d5c0e786c5b154a8b3821aee2d
Rostislav Brož <75474651+scraptechguy@users.noreply.github.com>
2021-03-02 09:21:08 +0100

    Update Fireworks.py

--- Fireworks.py
+++ Fireworks.py
@@ -69,7 +69,7 @@ purpleRocket2.penup()
 purpleRocket2.color("purple")
 purpleRocket2.shape("square")
 purpleRocket2.shapesize(0.5, 0.5)
-purpleRocket2.goto(300, -50)
+purpleRocket2.goto(300, 150)
 
 purpleRocket3 = turtle.Turtle()
 purpleRocket3.hideturtle()
@@ -77,7 +77,7 @@ purpleRocket3.penup()
 purpleRocket3.color("purple")
 purpleRocket3.shape("square")
 purpleRocket3.shapesize(0.5, 0.5)
-purpleRocket3.goto(300, -50)
+purpleRocket3.goto(300, 150)
 
 purpleRocket4 = turtle.Turtle()
 purpleRocket4.hideturtle()
@@ -85,7 +85,7 @@ purpleRocket4.penup()
 purpleRocket4.color("purple")
 purpleRocket4.shape("square")
 purpleRocket4.shapesize(0.5, 0.5)
-purpleRocket4.goto(300, -50)
+purpleRocket4.goto(300, 150)
 
 purpleRocket5 = turtle.Turtle()
 purpleRocket5.hideturtle()
@@ -93,7 +93,7 @@ purpleRocket5.penup()
 purpleRocket5.color("purple")
 purpleRocket5.shape("square")
 purpleRocket5.shapesize(0.5, 0.5)
-purpleRocket5.goto(300, -50)
+purpleRocket5.goto(300, 150)
 
 purpleRocket6 = turtle.Turtle()
 purpleRocket6.hideturtle()
@@ -101,7 +101,7 @@ purpleRocket6.penup()
 purpleRocket6.color("purple")
 purpleRocket6.shape("square")
 purpleRocket6.shapesize(0.5, 0.5)
-purpleRocket6.goto(300, -50)
+purpleRocket6.goto(300, 150)
 
 purpleRocket7 = turtle.Turtle()
 purpleRocket7.hideturtle()
@@ -109,7 +109,7 @@ purpleRocket7.penup()
 purpleRocket7.color("purple")
 purpleRocket7.shape("square")
 purpleRocket7.shapesize(0.5, 0.5)
-purpleRocket7.goto(300, -50)
+purpleRocket7.goto(300, 150)
 
 purpleRocket8 = turtle.Turtle()
 purpleRocket8.hideturtle()
@@ -117,7 +117,66 @@ purpleRocket8.penup()
 purpleRocket8.color("purple")
 purpleRocket8.shape("square")
 purpleRocket8.shapesize(0.5, 0.5)
-purpleRocket8.goto(300, -50)
+purpleRocket8.goto(300, 150)
+
+
+
+
+redRocket2 = turtle.Turtle()
+redRocket2.hideturtle()
+redRocket2.penup()
+redRocket2.color("red")
+redRocket2.shape("square")
+redRocket2.shapesize(0.5, 0.5)
+redRocket2.goto(300, 150)
+
+redRocket3 = turtle.Turtle()
+redRocket3.hideturtle()
+redRocket3.penup()
+redRocket3.color("red")
+redRocket3.shape("square")
+redRocket3.shapesize(0.5, 0.5)
+redRocket3.goto(300, 150)
+
+redRocket4 = turtle.Turtle()
+redRocket4.hideturtle()
+redRocket4.penup()
+redRocket4.color("red")
+redRocket4.shape("square")
+redRocket4.shapesize(0.5, 0.5)
+redRocket4.goto(300, 150)
+
+redRocket5 = turtle.Turtle()
+redRocket5.hideturtle()
+redRocket5.penup()
+redRocket5.color("red")
+redRocket5.shape("square")
+redRocket5.shapesize(0.5, 0.5)
+redRocket5.goto(300, 150)
+
+redRocket6 = turtle.Turtle()
+redRocket6.hideturtle()
+redRocket6.penup()
+redRocket6.color("red")
+redRocket6.shape("square")
+redRocket6.shapesize(0.5, 0.5)
+redRocket6.goto(300, 150)
+
+redRocket7 = turtle.Turtle()
+redRocket7.hideturtle()
+redRocket7.penup()
+redRocket7.color("red")
+redRocket7.shape("square")
+redRocket7.shapesize(0.5, 0.5)
+redRocket7.goto(300, 150)
+
+redRocket8 = turtle.Turtle()
+redRocket8.hideturtle()
+redRocket8.penup()
+redRocket8.color("red")
+redRocket8.shape("square")
+redRocket8.shapesize(0.5, 0.5)
+redRocket8.goto(300, 150)
 
 # ________________________________________
 
@@ -143,68 +202,69 @@ blueRocket7.showturtle()
 blueRocket8.showturtle()
 
 
+
 for i in range(30):
-    by2 = blueRocket2.ycor()
-    by2 += 2
-    blueRocket2.sety(by2)
+	by2 = blueRocket2.ycor()
+	by2 += 2
+	blueRocket2.sety(by2)
 
-    bx2 = blueRocket2.xcor()
-    bx2 += 4
-    blueRocket2.setx(bx2)
-
-
-    by3 = blueRocket3.ycor()
-    by3 += 2
-    blueRocket3.sety(by3)
-
-    bx3 = blueRocket3.xcor()
-    bx3 -= 4
-    blueRocket3.setx(bx3)
+	bx2 = blueRocket2.xcor()
+	bx2 += 4
+	blueRocket2.setx(bx2)
 
 
-    by4 = blueRocket4.ycor()
-    by4 += 2
-    blueRocket4.sety(by4)
+	by3 = blueRocket3.ycor()
+	by3 += 2
+	blueRocket3.sety(by3)
 
-    bx4 = blueRocket4.xcor()
-    bx4 += 3
-    blueRocket4.setx(bx4)
-
-
-    by5 = blueRocket5.ycor()
-    by5 += 2
-    blueRocket5.sety(by5)
-
-    bx5 = blueRocket5.xcor()
-    bx5 -= 4
-    blueRocket5.setx(bx5)
+	bx3 = blueRocket3.xcor()
+	bx3 -= 4
+	blueRocket3.setx(bx3)
 
 
-    by6 = blueRocket6.ycor()
-    by6 -= 2
-    blueRocket6.sety(by6)
+	by4 = blueRocket4.ycor()
+	by4 += 2
+	blueRocket4.sety(by4)
 
-    bx6 = blueRocket6.xcor()
-    bx6 -= 3
-    blueRocket6.setx(bx6)
-
-
-    by7 = blueRocket7.ycor()
-    by7 -= 1
-    blueRocket7.sety(by7)
-
-    bx7 = blueRocket7.xcor()
-    bx7 += 2
-    blueRocket7.setx(bx7)
+	bx4 = blueRocket4.xcor()
+	bx4 += 3
+	blueRocket4.setx(bx4)
 
 
-    by8 = blueRocket8.ycor()
-    by8 += 5
-    blueRocket8.sety(by8)
+	by5 = blueRocket5.ycor()
+	by5 += 2
+	blueRocket5.sety(by5)
 
-    bx8 = blueRocket8.xcor()
-    bx8 -= 1
-    blueRocket8.setx(bx8)
+	bx5 = blueRocket5.xcor()
+	bx5 -= 4
+	blueRocket5.setx(bx5)
+
+
+	by6 = blueRocket6.ycor()
+	by6 -= 2
+	blueRocket6.sety(by6)
+
+	bx6 = blueRocket6.xcor()
+	bx6 -= 3
+	blueRocket6.setx(bx6)
+
+
+	by7 = blueRocket7.ycor()
+	by7 -= 1
+	blueRocket7.sety(by7)
+
+	bx7 = blueRocket7.xcor()
+	bx7 += 2
+	blueRocket7.setx(bx7)
+
+
+	by8 = blueRocket8.ycor()
+	by8 += 5
+	blueRocket8.sety(by8)
+
+	bx8 = blueRocket8.xcor()
+	bx8 -= 1
+	blueRocket8.setx(bx8)
 
 blueRocket.hideturtle()
 blueRocket2.hideturtle()
@@ -226,7 +286,7 @@ purpleRocket.penup()
 purpleRocket.goto(300, -450)
 purpleRocket.showturtle()
 purpleRocket.left(90)
-purpleRocket.forward(400)
+purpleRocket.forward(600)
 purpleRocket.hideturtle()
 
 purpleRocket2.showturtle()
@@ -239,67 +299,67 @@ purpleRocket8.showturtle()
 
 for i in range(30):
 
-    py2 = purpleRocket2.ycor()
-    py2 += 2
-    purpleRocket2.sety(py2)
+	py2 = purpleRocket2.ycor()
+	py2 += 2
+	purpleRocket2.sety(py2)
 
-    px2 = purpleRocket2.xcor()
-    px2 += 4
-    purpleRocket2.setx(px2)
-
-
-    py3 = purpleRocket3.ycor()
-    py3 += 2
-    purpleRocket3.sety(py3)
-
-    px3 = purpleRocket3.xcor()
-    px3 -= 4
-    purpleRocket3.setx(px3)
+	px2 = purpleRocket2.xcor()
+	px2 += 4
+	purpleRocket2.setx(px2)
 
 
-    py4 = purpleRocket4.ycor()
-    py4 += 2
-    purpleRocket4.sety(py4)
+	py3 = purpleRocket3.ycor()
+	py3 += 2
+	purpleRocket3.sety(py3)
 
-    px4 = purpleRocket4.xcor()
-    px4 += 3
-    purpleRocket4.setx(px4)
-
-
-    py5 = purpleRocket5.ycor()
-    py5 += 2
-    purpleRocket5.sety(py5)
-
-    px5 = purpleRocket5.xcor()
-    px5 -= 4
-    purpleRocket5.setx(px5)
+	px3 = purpleRocket3.xcor()
+	px3 -= 4
+	purpleRocket3.setx(px3)
 
 
-    py6 = purpleRocket6.ycor()
-    py6 -= 2
-    purpleRocket6.sety(py6)
+	py4 = purpleRocket4.ycor()
+	py4 += 2
+	purpleRocket4.sety(py4)
 
-    px6 = purpleRocket6.xcor()
-    px6 -= 3
-    purpleRocket6.setx(px6)
-
-
-    py7 = purpleRocket7.ycor()
-    py7 -= 1
-    purpleRocket7.sety(py7)
-
-    px7 = purpleRocket7.xcor()
-    px7 += 2
-    purpleRocket7.setx(px7)
+	px4 = purpleRocket4.xcor()
+	px4 += 3
+	purpleRocket4.setx(px4)
 
 
-    py8 = purpleRocket8.ycor()
-    py8 += 5
-    purpleRocket8.sety(py8)
+	py5 = purpleRocket5.ycor()
+	py5 += 2
+	purpleRocket5.sety(py5)
 
-    px8 = purpleRocket8.xcor()
-    px8 -= 1
-    purpleRocket8.setx(px8)
+	px5 = purpleRocket5.xcor()
+	px5 -= 4
+	purpleRocket5.setx(px5)
+
+
+	py6 = purpleRocket6.ycor()
+	py6 -= 2
+	purpleRocket6.sety(py6)
+
+	px6 = purpleRocket6.xcor()
+	px6 -= 3
+	purpleRocket6.setx(px6)
+
+
+	py7 = purpleRocket7.ycor()
+	py7 -= 1
+	purpleRocket7.sety(py7)
+
+	px7 = purpleRocket7.xcor()
+	px7 += 2
+	purpleRocket7.setx(px7)
+
+
+	py8 = purpleRocket8.ycor()
+	py8 += 5
+	purpleRocket8.sety(py8)
+
+	px8 = purpleRocket8.xcor()
+	px8 -= 1
+	purpleRocket8.setx(px8)
 
 
 purpleRocket.hideturtle()
@@ -312,7 +372,6 @@ purpleRocket7.hideturtle()
 purpleRocket8.hideturtle()
 
 
-while True:
-    wn.update()
+
 
 
